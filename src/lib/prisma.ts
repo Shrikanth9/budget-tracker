da590c2748348b7ee76773a6200e7eda9c2e7d0b
Shrikanth <shrikanthbhat30@gmail.com>
2025-08-03 20:31:17 +0530

    Setup prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@/generated/prisma/client";
+
+export const db = (globalThis as any).prisma || new PrismaClient();
+
+if(process.env.NODE_ENV !== "production") {
+  (globalThis as any).prisma = db;
+}
