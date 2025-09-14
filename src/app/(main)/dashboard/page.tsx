39bc19a727af3f2a36c6014adbfda3096d614127
Shrikanth <shrikanthbhat30@gmail.com>
2025-09-14 12:31:54 +0530

    add transaction overview on dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { getUserAccounts } from '@/actions/dashboard'
+import { getDashboardData, getUserAccounts } from '@/actions/dashboard'
 import CreateAccountDrawer from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
@@ -6,6 +6,7 @@ import React from 'react'
 import AccountCard from './_components/account-card'
 import { getCurrentBudget } from '@/actions/budgets'
 import BudgetProgress from './_components/budget-progress'
+import { DashboardOverview } from '../transaction/_components/transaction-overview'
 
 const DashboardPage = async() => {
   const accounts = await getUserAccounts()  
@@ -17,6 +18,8 @@ const DashboardPage = async() => {
     budgetData = await getCurrentBudget(defaultAccount.id)
   }  
 
+  const transactions = await getDashboardData();
+
   return (
     <div className='space-y-8'>
       { defaultAccount && (
@@ -25,6 +28,12 @@ const DashboardPage = async() => {
             currentExpenses={budgetData?.currentExpenses || 0}
          />
       )}
+
+      <DashboardOverview
+        accounts={accounts}
+        transactions={transactions || []}
+      />
+      
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-4'>
         <CreateAccountDrawer>
           <Card className='hover:shadow-md transition-shadow cursor-pointer border-dashed'>
