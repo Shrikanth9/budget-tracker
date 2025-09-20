6abc0c293c730350e124dc9cf566fcfb70bc710b
Shrikanth <shrikantha.ma@juegostudioz.com>
2025-09-20 18:39:15 +0530

    Revert "adding arcjet middleware shield"
    This reverts commit fbc9a693c94cd73b9200678980b39ed71d795288.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -7,32 +6,15 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-const clerk = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if(!userId && isProtectedRoute(req)) {
     const {redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
+   
 });
-
-  const aj = arcjet({
-    key: process.env.ARCJET_API_KEY as string,
-    rules: [
-       shield({
-          mode: "LIVE"
-       }),
-       detectBot({
-          mode: "LIVE",
-          allow: [
-            "CATEGORY:SEARCH_ENGINE",
-            "GO_HTTP"
-          ]
-       })
-    ]
-  });
-
-export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
