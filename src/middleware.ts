fbc9a693c94cd73b9200678980b39ed71d795288
Shrikanth <shrikantha.ma@juegostudioz.com>
2025-09-20 16:24:38 +0530

    adding arcjet middleware shield

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -6,15 +7,32 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if(!userId && isProtectedRoute(req)) {
     const {redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-   
 });
+
+  const aj = arcjet({
+    key: process.env.ARCJET_API_KEY as string,
+    rules: [
+       shield({
+          mode: "LIVE"
+       }),
+       detectBot({
+          mode: "LIVE",
+          allow: [
+            "CATEGORY:SEARCH_ENGINE",
+            "GO_HTTP"
+          ]
+       })
+    ]
+  });
+
+export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
